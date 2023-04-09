84a6caa728d13139eabcddcabb2533445a49c52c
abhimanyu121993 <softpromani@gmail.com>
2023-04-09 12:20:26 +0530

    manish

--- app/Http/Controllers/shop/ShopDetailsController.php
+++ app/Http/Controllers/shop/ShopDetailsController.php
@@ -63,7 +63,7 @@ class ShopDetailsController extends Controller
             'vaddress'=>'required',
             'signature'=>'required',
            ]);
-           VendorDetail::find(Auth::guard(Helper::getGuard())->id())->update([
+           VendorDetail::where('vendore_id',Auth::guard(Helper::getGuard())->id())->update([
             'time_zone'=>$request->timezone,
             'currency'=>$request->currency,
             'vendor_name'=>$request->name,
