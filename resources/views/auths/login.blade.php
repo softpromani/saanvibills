ee1ac5ad03262b6119c0ac8bb24de5dc59d484c2
abhimanyu121993 <softpromani@gmail.com>
2023-04-18 21:45:10 +0530

    bill

--- resources/views/auths/login.blade.php
+++ resources/views/auths/login.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
-    <title>SignIn Boxed | CORK - Multipurpose Bootstrap Dashboard Template </title>
+    <title>Invoice :: Innovation Trove </title>
     <link rel="icon" type="image/x-icon" href="../src/assets/img/favicon.ico"/>
     <link href="{{asset('assets/layouts/collapsible-menu/css/light/loader.css')}}" rel="stylesheet" type="text/css" />
     <link href="{{asset('assets/layouts/collapsible-menu/css/dark/loader.css')}}" rel="stylesheet" type="text/css" />
