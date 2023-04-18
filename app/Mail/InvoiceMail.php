ee4abce991026215ebde3a409dfc05f309973528
abhimanyu121993 <softpromani@gmail.com>
2023-04-18 21:42:56 +0530

    bill

--- app/Mail/InvoiceMail.php
+++ app/Mail/InvoiceMail.php
@@ -5,6 +5,7 @@ namespace App\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Attachment;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
@@ -32,7 +33,7 @@ class InvoiceMail extends Mailable
     public function envelope()
     {
         return new Envelope(
-            subject: 'Invoice Mail',
+            subject: 'Invoice From '.$this->bill->customer->name,
         );
     }
 
@@ -58,6 +59,8 @@ class InvoiceMail extends Mailable
      */
     public function attachments()
     {
-        return [];
+        return [
+            Attachment::fromPath(public_path('assets/compu.jpg'))
+        ];
     }
 }
